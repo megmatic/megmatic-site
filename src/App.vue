f2e61d18c2faded343d4969ddf9063e66c504d12
Meg <meggoodwin57@gmail.com>
2021-06-06 11:37:14 -0500

    set up dev environment with vue cli and tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>hello, megmatic</h1>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+  },
+};
 </script>
 
 <style>
